78e08eee1760e6a20a92713fd00ff99d2983b7f1
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2024-06-14 01:44:18 +0300

    task_3

---
+++ task_3.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def decrypt_message(encrypted_vector, key_matrix):
+    eigenvalues, eigenvectors = np.linalg.eig(key_matrix)
+    diagonalized_key_matrix = np.dot(np.dot(eigenvectors, np.diag(eigenvalues)), np.linalg.inv(eigenvectors))
+    decrypted_vector = np.dot(np.linalg.inv(diagonalized_key_matrix), encrypted_vector)
+    decrypted_message = ''.join([chr(int(round(value.real))) for value in decrypted_vector])
+    return decrypted_message
+
+
+def encrypt_message(message, key_matrix):
+    message_vector = np.array([ord(char) for char in message])
+    eigenvalues, eigenvectors = np.linalg.eig(key_matrix)
+    diagonalized_key_matrix = np.dot(np.dot(eigenvectors, np.diag(eigenvalues)), np.linalg.inv(eigenvectors))
+    encrypted_vector = np.dot(diagonalized_key_matrix, message_vector)
+    return encrypted_vector
+
+
+# Example usage
+key_matrix = np.random.randint(0, 256, (len("Hello, World!"), len("Hello, World!")))
+
+original_message = "Hello, World!"
+print("Original Message:", original_message)
+
+encrypted_vector = encrypt_message(original_message, key_matrix)
+print("Encrypted Message:", encrypted_vector)
+
+decrypted_message = decrypt_message(encrypted_vector, key_matrix)
+print("Decrypted Message:", decrypted_message)
