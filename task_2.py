ca452ff56314a9c402a5c1449bdbd776e6e5ccad
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2024-06-15 02:36:19 +0300

    report was added

--- task_2.py
+++ task_2.py
@@ -3,11 +3,10 @@ from matplotlib.image import imread
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA, IncrementalPCA
 
-# Step 1: Load and display the original image
+# крок 1: вивести початкове кольорове зображення та вектор
 image_raw = imread("image.png")
-print("Original image shape:", image_raw.shape)  # Dimensions of the image (height, width, channels)
+print("Original image shape:", image_raw.shape)  # розміри зображення в пікселях та кількість основних каналів кольорів (height, width, channels)
 
-# Displaying the original image
 plt.figure(figsize=[12, 8])
 plt.imshow(image_raw)
 plt.title("Original Image")
@@ -17,29 +16,26 @@ plt.show()
 image_dimensions = np.array([image_raw.shape[0], image_raw.shape[1], image_raw.shape[2]])
 print("Image dimensions and number of color channels:", image_dimensions)
 
-# Step 2: Convert to grayscale and display the grayscale image
+# крок 2: Перетворити зображення в чорно-біле та вивести розмір зображення і кількість каналів кольорів
 image_sum = image_raw.sum(axis=2)  # Sum across the color channels
+print(image_sum.shape)
 image_bw = image_sum / image_sum.max()  # Normalize to the range [0, 1]
 print("Grayscale image shape:", image_bw.shape)  # Dimensions of the grayscale image
 
-# Displaying the grayscale image
 plt.figure(figsize=[12, 8])
 plt.imshow(image_bw, cmap=plt.cm.gray)
 plt.title("Grayscale Image")
 plt.show()
 
-# Step 3: Apply PCA to the grayscale image
+# крок 3: Застосувати PCA для матриці компонентів image_bw
 pca = PCA()
 pca.fit(image_bw)
 
-# Cumulative variance
+# cumulative variance
 var_cumu = np.cumsum(pca.explained_variance_ratio_) * 100
-
-# Number of components to explain 95% variance
-k = np.argmax(var_cumu >= 95) + 1  # Adding 1 as np.argmax returns the first index where condition is met
+k = np.argmax(var_cumu >= 95) + 1
 print("Number of components explaining 95% variance:", k)
 
-# Plot cumulative explained variance
 plt.figure(figsize=[10, 5])
 plt.plot(var_cumu)
 plt.axhline(y=95, color='r', linestyle='--')
@@ -49,25 +45,28 @@ plt.xlabel('Number of Principal Components')
 plt.ylabel('Cumulative Explained Variance (%)')
 plt.show()
 
-# Step 4: Reconstruct the grayscale image using the identified number of components
+# крок 4: Провести реконструкцію чорно-білого зображення, використовуючи обмежену кількість компонентів, знайдену в попередньому кроці
 ipca = IncrementalPCA(n_components=k)
 image_recon = ipca.inverse_transform(ipca.fit_transform(image_bw))
 
-# Displaying the reconstructed image
+# Вивести отримане зображення
 plt.figure(figsize=[12, 8])
 plt.imshow(image_recon, cmap=plt.cm.gray)
 plt.title(f"Reconstructed Image with {k} Components (95% Variance)")
 plt.show()
 
-# Step 5: Reconstruct and display the grayscale image using different numbers of components
+
+# крок 5: Проведіть реконструкцію зображення для різної кількості компонент та виведіть відповідні результати
+
+
 def plot_at_k(k):
     ipca = IncrementalPCA(n_components=k)
     image_recon = ipca.inverse_transform(ipca.fit_transform(image_bw))
     plt.imshow(image_recon, cmap=plt.cm.gray)
     plt.title(f"Components: {k}")
 
-ks = [10, 25, 50, 100, 150, 250]
 
+ks = [10, 25, 50, 100, 150, 250]
 plt.figure(figsize=[15, 9])
 
 for i in range(len(ks)):
--- task_3.py
+++ task_3.py
@@ -1,14 +1,6 @@
 import numpy as np
 
-
-def decrypt_message(encrypted_vector, key_matrix):
-    eigenvalues, eigenvectors = np.linalg.eig(key_matrix)
-    diagonalized_key_matrix = np.dot(np.dot(eigenvectors, np.diag(eigenvalues)), np.linalg.inv(eigenvectors))
-    decrypted_vector = np.dot(np.linalg.inv(diagonalized_key_matrix), encrypted_vector)
-    decrypted_message = ''.join([chr(int(round(value.real))) for value in decrypted_vector])
-    return decrypted_message
-
-
+#ф-я кодування
 def encrypt_message(message, key_matrix):
     message_vector = np.array([ord(char) for char in message])
     eigenvalues, eigenvectors = np.linalg.eig(key_matrix)
@@ -17,7 +9,16 @@ def encrypt_message(message, key_matrix):
     return encrypted_vector
 
 
-# Example usage
+#ф-я розкодування
+def decrypt_message(encrypted_vector, key_matrix):
+    eigenvalues, eigenvectors = np.linalg.eig(key_matrix)
+    diagonalized_key_matrix = np.dot(np.dot(eigenvectors, np.diag(eigenvalues)), np.linalg.inv(eigenvectors))
+    decrypted_vector = np.dot(np.linalg.inv(diagonalized_key_matrix), encrypted_vector)
+    decrypted_message = ''.join([chr(int(round(value.real))) for value in decrypted_vector])
+    return decrypted_message
+
+
+# Генерація матриці ключа
 key_matrix = np.random.randint(0, 256, (len("Hello, World!"), len("Hello, World!")))
 
 original_message = "Hello, World!"
